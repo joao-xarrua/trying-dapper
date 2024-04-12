ffe99b93db2eec5b46eb7ee09a9b1129a4b421e9
root <root@xarras>
2024-04-12 10:21:14 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntendendoDapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
+        }
+    }
+}
